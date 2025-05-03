63d6d0b913abd04802d542cfed47922c23f4d9e5
Assassin33GT <bodeidris33@gmail.com>
2025-05-03 15:58:26 +0300

    home_page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  static final String id = "Home fwfw Page";
+  static final String id = "Home Page";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home Page"),),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle), label: "Profile"),
+        ],
+      ),
     );
   }
 }
