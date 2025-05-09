20130ec9e12944c231636635ba63101b355de60f
Assassin33GT <bodeidris33@gmail.com>
2025-05-09 20:40:02 +0300

    productpage

--- lib/views/product_page.dart
+++ lib/views/product_page.dart
@@ -3,7 +3,11 @@ import 'package:ecomm/view_models/product_view_model.dart';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatefulWidget {
-  const ProductPage({super.key, required this.product, required this.productViewModel});
+  const ProductPage({
+    super.key,
+    required this.product,
+    required this.productViewModel,
+  });
   final Product product;
   final ProductViewModel productViewModel;
   @override
@@ -14,9 +18,7 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-      ),
+      appBar: AppBar(backgroundColor: Colors.green),
       backgroundColor: const Color(0xFFF5F5F5),
       body: Padding(
         padding: const EdgeInsets.only(left: 18.0, right: 18.0),
@@ -53,16 +55,43 @@ class _ProductPageState extends State<ProductPage> {
                     ),
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    "x${widget.product.quantity}",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 183, 169, 44),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
                 Expanded(
                   child: Align(
                     alignment: Alignment.centerRight,
-                    child: Text(
-                      "⭐${widget.product.rating}",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "⭐${widget.product.rating}",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 3.0),
+                          child: Text(
+                            "(${widget.product.ratingCount})",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -102,35 +131,63 @@ class _ProductPageState extends State<ProductPage> {
                       width: double.infinity,
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: widget.productViewModel.checkProductInCart(widget.product)?Colors.green:Colors.white70,
+                          backgroundColor:
+                              widget.productViewModel.checkProductInCart(
+                                    widget.product,
+                                  )
+                                  ? Colors.green
+                                  : Colors.white70,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20),
                           ),
                         ),
                         onPressed: () {
                           setState(() {
-                            if(widget.productViewModel.checkProductInCart(widget.product)){
-                            widget.productViewModel.removeProductFromCart(widget.product);
-                          }else{
-                            widget.productViewModel.addProductToCart(widget.product);
-                          }
+                            if (widget.productViewModel.checkProductInCart(
+                              widget.product,
+                            )) {
+                              widget.productViewModel.removeProductFromCart(
+                                widget.product,
+                              );
+                            } else {
+                              widget.productViewModel.addProductToCart(
+                                widget.product,
+                              );
+                            }
                           });
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                            content: widget.productViewModel.checkProductInCart(widget.product)? Text("Product added to cart"): Text("Product removed from cart"),
-                            duration: Duration(milliseconds: 600),
-                            backgroundColor: Colors.green,
-                          ),
-                          );
+                          // ScaffoldMessenger.of(context).showSnackBar(
+                          //   SnackBar(
+                          //     content:
+                          //         widget.productViewModel.checkProductInCart(
+                          //               widget.product,
+                          //             )
+                          //             ? Text("Product added to cart")
+                          //             : Text("Product removed from cart"),
+                          //     duration: Duration(milliseconds: 600),
+                          //     backgroundColor: Colors.green,
+                          //   ),
+                          // );
                         },
-                        child: Text(
-                          'Add To Cart',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: widget.productViewModel.checkProductInCart(widget.product)?Colors.white:Colors.black,
-                          ),
-                        ),
+                        child:
+                            !widget.productViewModel.checkProductInCart(
+                                  widget.product,
+                                )
+                                ? Text(
+                                  'Add To Cart',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                )
+                                : Text(
+                                  "Remove From Cart",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
                       ),
                     ),
                   ),
